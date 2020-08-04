a019cc40656403affe34d7987f7bfb47153ffa25
dbpatnodee <dbpatnode@hotmail.com>
2020-08-04 16:54:48 -0700

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,23 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
+  
+  def status
+    if self.active == true
+      # self.update(active: false)
+      "This student is currently active."
+    else 
+      # self.update(active: true)
+      "This student is currently inactive."
+    end
+  end
+
+  def toggle_activate
+    if self.active == true 
+      self.update(active: false)
+    else
+      self.update(active: true)
+    end
+      self.save
+  end
 end
